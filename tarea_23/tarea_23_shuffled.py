f400d840c0c956c35ca03a15f97fcfdc8fa7fd33
Diego Lasa <ethoreum@protonmail.com>
2020-07-14 10:41:07 +0200

    Make main simpler

--- tarea_23/tarea_23_shuffled.py
+++ tarea_23/tarea_23_shuffled.py
@@ -1,24 +1,20 @@
 from defs import *
 
 def main():
-
     input_text = normalize_input(sys.argv[1])
     deck = generate_shuffled_deck()
     keystream = generate_keystream(input_text, deck)
     
     if sys.argv[2] == 'encrypt':
         cypher = encrypt_text(keystream, input_text)
-        plain_text = decrypt_text(keystream, ''.join(cypher))
-        print("Texto sin cifrar: ", ''.join(plain_text))
+        print("Texto sin cifrar: ", ''.join(sys.argv[1]))
         print("Clave de cifrado: ", keystream_to_char(keystream))
         print("Texto cifrado   : ", ''.join(cypher))
     elif sys.argv[2] == 'decrypt':
         plain_text = decrypt_text(keystream, input_text)
-        cypher = encrypt_text(keystream, ''.join(plain_text))
-        print("Texto cifrado   : ", ''.join(cypher))
+        print("Texto cifrado   : ", ''.join(sys.argv[1])
         print("Clave de cifrado: ", keystream_to_char(keystream))
         print("Texto sin cifrar: ", ''.join(plain_text))
-        
 main()
 
 
