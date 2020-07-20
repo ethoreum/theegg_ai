323c8e530ce60871db12928215fd1d2e953ceb4e
Diego Lasa <ethoreum@protonmail.com>
2020-07-20 09:28:11 +0200

    Change loop lower bound and add exception for 0

--- tarea_24/tarea_24_positivos.py
+++ tarea_24/tarea_24_positivos.py
@@ -1,25 +1,25 @@
 import sys 
 
 def check_and_assign_input():
-    """ Esta función checkea que el input es un número"""
+    """ Esta función checkea que el input es un número entero"""
     try:
         int(sys.argv[1])
     except ValueError:
-        print('Not a integer number')
+        print('Not an integer number')
     else:
         return int(sys.argv[1])
-
 
 def decimal_to_binary(decimal_number):
     """Esta función convierte un número decimal a su
     representación binaria"""
     bit_list=[]
-
-    while decimal_number > 1:
-        remainder = decimal_number % 2
-        decimal_number  = int(decimal_number/2)
-        bit_list.append(remainder)
-    bit_list.append(1)
+    if decimal_number > 0:
+        while decimal_number > 0:
+            remainder = decimal_number % 2
+            decimal_number  = decimal_number // 2
+            bit_list.append(remainder)
+    else:
+        bit_list.append(0)
  
     # Imprimir elementos de una lista sin separación en orden inverso
     print(*bit_list[::-1], sep="")
@@ -28,6 +28,7 @@ def main():
     decimal_number = check_and_assign_input()
     decimal_to_binary(decimal_number)
 
-main()
+if __name__ == "__main__":
+    main()
 
 
