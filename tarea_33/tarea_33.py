ca7795ee54a02a8a228ef8da4bccaf01f38a3b9b
Diego Lasa <ethoreum@protonmail.com>
2020-07-21 13:11:56 +0200

    Replace tabs with whitespaces in tarea_33.py

--- tarea_33/tarea_33.py
+++ tarea_33/tarea_33.py
@@ -1,29 +1,25 @@
 class Pokemon():
-
-	def __init__(self, attack, hp):
-		self.attack = attack
-		self.hp = hp
+    def __init__(self, attack, hp):
+        self.attack = attack
+        self.hp = hp
 
 def main():
+    pikachu = Pokemon(55, 100)
+    jygglypuff = Pokemon(45, 100)
+    turn = 1
 
-	pikachu = Pokemon(55, 100)
-	jygglypuff = Pokemon(45, 100)
-	turn = 1
+    while pikachu.hp > 0 and jygglypuff.hp > 0:
+        if turn == 1:
+            jygglypuff.hp -= pikachu.attack
+            turn = 0
+        else:
+            pikachu.hp -= jygglypuff.attack
+            turn = 1
 
-	while pikachu.hp > 0 and jygglypuff.hp > 0:
-		if turn == 1:
-			jygglypuff.hp -= pikachu.attack
-			turn = 0
-		else:
-			pikachu.hp -= jygglypuff.attack
-			turn = 1
-
-	if jygglypuff.hp <= 0:
-		print("Pikachu Wins!")
-	else:
-		print("Jygglypuff Wins!")
+    if jygglypuff.hp <= 0:
+        print("Pikachu Wins!")
+    else:
+        print("Jygglypuff Wins!")
 
 if __name__ == "__main__":
     main()
-
-
